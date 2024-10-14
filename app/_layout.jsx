107835d224a1391dd0087d497eb49cdcf4d393ee
Farrukh <contact@farrukh.dev>
2024-10-14 21:28:12 +0100

    added splash screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Apprenticeship Watch</Text>
+      <Text>Apprentice Watch</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FF8040",
+    alignItems: "center",
+    justifyContent: "center",
   },
-});
+})
