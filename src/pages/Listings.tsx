43ad6e3e726789895c1313d72f083416a4b7ad52
Grid <147248100+hhussain04@users.noreply.github.com>
2025-02-13 20:02:55 +0000

    Fix/listing page (#16)
    * Add padding to Navbar component for improved layout
    
    * Add Cookie Policy page and update footer link
    
    * Update Cookie Policy page for clarity and improved user guidance
    
    * Fix color issue caused by padding introduction by adding Tailwind CSS styling to the padding div.
    
    * Change background color of Listings page to orange for improved visibility and match website color theme
    
    * Update Listings page background to a gradient for enhanced visual appeal

--- src/pages/Listings.tsx
+++ src/pages/Listings.tsx
@@ -70,7 +70,7 @@ export const Listings = () => {
         />
       </Helmet>
 
-      <div className="min-h-screen pt-24 pb-12 bg-gray-50 dark:bg-gray-900">
+      <div className="min-h-screen pt-24 pb-12 bg-gradient-to-b from-orange-50  dark:bg-gray-900">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-8">
             Available Apprenticeships
