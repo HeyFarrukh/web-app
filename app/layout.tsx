c3e1f6953945da7c73cd975bc696ce4adfd4661d
Farrukh <contact@farrukh.dev>
2025-02-17 22:55:26 +0000

    Feature/speed insights vercel (#26)
    * Minor modifications to gitignore
    
    * added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter, Playfair_Display } from 'next/font/google';
 import { Navbar } from '@/components/Navbar';
 import { Footer } from '@/components/Footer';
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from '@/services/analytics/analytics';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -128,6 +128,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
