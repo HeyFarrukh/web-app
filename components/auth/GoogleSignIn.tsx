73533b734458f150a729aa23ccd93cdb778b71d6
STENTORS <ezraibaldwin@gmail.com>
2025-04-07 13:28:44 +0100

    optimise-cv signin flow fixed

--- components/auth/GoogleSignIn.tsx
+++ components/auth/GoogleSignIn.tsx
@@ -1,5 +1,5 @@
-import { useRouter, useSearchParams } from 'next/navigation';
-import { googleAuthService } from '@/services/auth/googleAuthService';
+import { useRouter, useSearchParams } from "next/navigation";
+import { googleAuthService } from "@/services/auth/googleAuthService";
 
 interface GoogleSignInProps {
   redirect?: string | null;
@@ -8,13 +8,14 @@ interface GoogleSignInProps {
 export const GoogleSignIn = ({ redirect }: GoogleSignInProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const redirectPath = redirect || searchParams?.get('redirect') || '/';
+  const redirectPath = redirect || searchParams?.get("redirect") || "/";
 
   const handleGoogleSignIn = async () => {
     try {
-      await googleAuthService.signIn();
+      console.log("Redirect path:", redirectPath);
+      await googleAuthService.signIn(redirectPath);
     } catch (error: any) {
-      console.error('Unexpected error during sign in:', error);
+      console.error("Unexpected error during sign in:", error);
     }
   };
 
@@ -23,7 +24,11 @@ export const GoogleSignIn = ({ redirect }: GoogleSignInProps) => {
       onClick={handleGoogleSignIn}
       className="bg-white border rounded-md px-6 py-2 shadow-md hover:shadow-lg transition flex items-center"
     >
-      <img src="/assets/google.svg" alt="Google Logo" className="w-5 h-5 mr-2" />
+      <img
+        src="/assets/google.svg"
+        alt="Google Logo"
+        className="w-5 h-5 mr-2"
+      />
       Continue with Google
     </button>
   );
